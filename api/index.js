1d51f2dde80da01d68a78cb5d7395c48118cc81e
luisnarv <115510650+luisnarv@users.noreply.github.com>
2023-02-17 18:40:32 -0500

    Backend

--- api/index.js
+++ api/index.js
@@ -26,24 +26,6 @@ const {PORT } = process.env
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, async () => {
-
-    const allCountries = Country.findAll();
-    if(!allCountries.length){
-    const apiCountriesResponse = await axios.get('https://restcountries.com/v3/all');
-    var apiCountries = apiCountriesResponse.data.map((e) => {
-      return {
-        id: e.cca3,
-        name: e.name.common,
-        image: e.flags[0],
-        continent: e.continents[0],
-        capital: e.capital ? e.capital[0] : 'Not found',
-        subregion: e.subregion,
-        area: e.area,
-        population: e.population
-      }
-    })
-        await Country.bulkCreate(apiCountries);
-  }
    
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
